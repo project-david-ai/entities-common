e14cd671bc0044dd8c6fa045e9bf41d0c0087e0f
Francisco <francis.neequaye@projectdavid.co.uk>
2026-02-25 00:13:09 -0300

    feat: add PLATFORM_TOOLS in plaform_tools.py

---
+++ src/projectdavid_common/constants/plaform_tools.py
@@ -0,0 +1,18 @@
+# src/projectdavid_common/constants/plaform_tools.py
+PLATFORM_TOOLS = [
+    "code_interpreter",
+    "web_search",
+    "search_web_page",
+    "scroll_web_page",
+    "read_web_page",
+    "perform_web_search",
+    "vector_store_search",
+    "computer",
+    "delegate_research_task",
+    "delegate_engineer_task",
+    "read_scratchpad",
+    "update_scratchpad",
+    "append_scratchpad",
+    "get_device_info",
+    "search_inventory_by_group",
+]
